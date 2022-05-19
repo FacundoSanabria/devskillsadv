10e5b26a36e938f9093f0d353f38387e8302ddda
FockaSanabria <sanabriafm@gmail.com>
2022-05-19 00:37:41 -0300

    other pages

--- Front/front/src/components/Error404.js
+++ Front/front/src/components/Error404.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Error404() {
     return (
-        <React.Fragment>
-            Error 404
-        </React.Fragment>
+        <div className='container p-3'>
+            <div className='row'>
+                <div className="card" id="main-card">
+                    <div className="card-body text-center">
+                        <h1>ERROR 404</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Error404;
--- Front/front/src/components/OtherPage/Other.js
+++ Front/front/src/components/OtherPage/Other.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Other() {
     return (
-        <React.Fragment>
-            Other Page
-        </React.Fragment>
+        <div className='container p-3'>
+            <div className='row'>
+                <div className="card" id="main-card">
+                    <div className="card-body text-center">
+                        <h1>OTHER PAGE</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Other;
